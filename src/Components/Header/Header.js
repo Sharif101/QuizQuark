4941be7ff770cf8ac67ff8b522c19b3fba43ed3d
Sharif101 <sharif009009@gmail.com>
2022-10-10 23:58:52 +0600

    addNavbar

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+// import NavDropdown from "react-bootstrap/NavDropdown";
+import "./Header.css";
+
 const Header = () => {
   return (
     <div>
-      <Link to="/">Home</Link>
-      <Link to="/blog">Blog</Link>
+      {/* <Link to="/">Home</Link>
+      <Link to="/blog">Blog</Link> */}
+
+      <Navbar className="nav">
+        <Container>
+          <Navbar.Brand className="nav_title">QuizQuark</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Link to="/">Home</Link>
+              <Link to="/blog">Blog</Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   );
 };
