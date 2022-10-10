c889cf9ef680344901e903e4f7596f43550f5a56
Sharif101 <sharif009009@gmail.com>
2022-10-11 02:53:05 +0600

    css added

--- src/Components/Question/Question.js
+++ src/Components/Question/Question.js
@@ -1,10 +1,19 @@
 import React from "react";
+import "./Question.css";
 
 const Question = ({ ques }) => {
-  let { question } = ques;
+  let { question, options } = ques;
   return (
-    <div>
-      <p>{question}</p>
+    <div className="container">
+      <div className="ques">
+        <h5 className="ques-title">{question}</h5>
+        <div className="options">
+          <p>{options[0]}</p>
+          <p>{options[1]}</p>
+          <p>{options[2]}</p>
+          <p>{options[3]}</p>
+        </div>
+      </div>
     </div>
   );
 };
