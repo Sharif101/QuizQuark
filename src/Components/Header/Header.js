b635e06473056e6228981e1c075248948c8cc2c9
Sharif101 <sharif009009@gmail.com>
2022-10-11 00:09:00 +0600

    nav bar edit

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-// import NavDropdown from "react-bootstrap/NavDropdown";
+import NavDropdown from "react-bootstrap/NavDropdown";
 import "./Header.css";
 
 const Header = () => {
@@ -13,14 +13,19 @@ const Header = () => {
       {/* <Link to="/">Home</Link>
       <Link to="/blog">Blog</Link> */}
 
-      <Navbar className="nav">
+      <Navbar className="nav" expand="lg">
         <Container>
-          <Navbar.Brand className="nav_title">QuizQuark</Navbar.Brand>
+          <Navbar.Brand className="nav_title" href="#home">
+            QuizQuark
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
               <Link to="/">Home</Link>
               <Link to="/blog">Blog</Link>
+              {/* <NavDropdown>
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              </NavDropdown> */}
             </Nav>
           </Navbar.Collapse>
         </Container>
