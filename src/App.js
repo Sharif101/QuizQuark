7ee160b14edf4fa5151b3530813b674dfcb9ef5a
Sharif101 <sharif009009@gmail.com>
2022-10-10 23:22:56 +0600

    router add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 // import logo from './logo.svg';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
+import Blog from "./Components/Blog/Blog";
+import Home from "./Components/Home.js/Home";
+import Main from "./layout/Main";
 
 function App() {
+  let router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        {
+          path: "/",
+          element: <Home></Home>,
+        },
+
+        {
+          path: "/blog",
+          element: <Blog></Blog>,
+        },
+      ],
+    },
+  ]);
   return (
-    <div className="App">
-      <h1>Quiz</h1>
+    <div>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
