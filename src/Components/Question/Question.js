0e8a20f064ae0862b682d0834805d237dfa12741
Sharif101 <sharif009009@gmail.com>
2022-10-11 11:20:27 +0600

    correct ans added

--- src/Components/Question/Question.js
+++ src/Components/Question/Question.js
@@ -1,17 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Question.css";
 
 const Question = ({ ques }) => {
-  let { question, options } = ques;
+  let { question, options, correctAnswer } = ques;
+
+  let [answer, setAnswer] = useState("");
+
+  let checkrslt = (e) => {
+    setAnswer(e);
+    if (answer !== correctAnswer) {
+      alert("wrong");
+    } else {
+      alert("correct");
+    }
+  };
   return (
     <div className="container">
       <div className="ques">
         <h5 className="ques-title">{question}</h5>
         <div className="options">
-          <p>{options[0]}</p>
+          <div className="inpt">
+            <input
+              onClick={(e) => checkrslt(e.target.value)}
+              type="radio"
+              name="check"
+              value={options[0]}
+            ></input>
+            <label>{options[0]}</label>
+          </div>
+          <div className="inpt">
+            <input
+              onClick={(e) => checkrslt(e.target.value)}
+              type="radio"
+              name="check"
+              value={options[1]}
+            ></input>
+            <label>{options[1]}</label>
+          </div>
+          <div className="inpt">
+            <input
+              onClick={(e) => checkrslt(e.target.value)}
+              type="radio"
+              name="check"
+              value={options[2]}
+            ></input>
+            <label>{options[2]}</label>
+          </div>
+          <div className="inpt">
+            <input
+              onClick={(e) => checkrslt(e.target.value)}
+              type="radio"
+              name="check"
+              value={options[3]}
+            ></input>
+            <label>{options[3]}</label>
+          </div>
+          {/* <p>{options[0]}</p>
           <p>{options[1]}</p>
           <p>{options[2]}</p>
-          <p>{options[3]}</p>
+          <p>{options[3]}</p> */}
         </div>
       </div>
     </div>
