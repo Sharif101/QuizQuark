a4ebe2337f367de5c8d0e10b88793856659d75e8
Sharif101 <sharif009009@gmail.com>
2022-10-11 14:52:59 +0600

    css added

--- src/Components/Questions/Questions.js
+++ src/Components/Questions/Questions.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
 import Question from "../Question/Question";
+import "./Questions.css";
 
 const Questions = () => {
-  let questions = useLoaderData().data.questions;
+  let Ques = useLoaderData();
+  let { name } = Ques.data;
+  let questions = Ques.data.questions;
   return (
-    <div>
+    <div className="container">
+      <div className="questions-title">
+        <h5>{name} Questions are here</h5>
+      </div>
+
       {questions.map((ques) => (
         <Question ques={ques}></Question>
       ))}
